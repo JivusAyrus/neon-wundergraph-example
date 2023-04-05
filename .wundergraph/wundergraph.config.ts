7f50b004f1771516fdf2ea43109b04c0510b2be5
JivusAyrus <suvijsurya76@gmail.com>
2023-04-05 18:20:09 +0530

    Initial Commit

---
+++ .wundergraph/wundergraph.config.ts
@@ -0,0 +1,27 @@
+import { configureWunderGraphApplication, cors, EnvironmentVariable, introspect, templates } from '@wundergraph/sdk';
+import server from './wundergraph.server';
+import operations from './wundergraph.operations';
+
+const db = introspect.postgresql({
+	apiNamespace: 'db',
+	databaseURL: new EnvironmentVariable('DATABASE_URL'),
+});
+
+// configureWunderGraph emits the configuration
+configureWunderGraphApplication({
+	apis: [db],
+	server,
+	operations,
+	codeGenerators: [
+		{
+			templates: [...templates.typescript.all],
+		},
+	],
+	cors: {
+		...cors.allowAll,
+		allowedOrigins: ['http://localhost:3000'],
+	},
+	security: {
+		enableGraphQLEndpoint: true,
+	},
+});
